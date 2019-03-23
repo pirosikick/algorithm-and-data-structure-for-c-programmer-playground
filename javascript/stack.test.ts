28ff46703e2a9cc7d5c28ccb5ce89e94d6f16fb8
Hiroyuki Anai <pirosikick@gmail.com>
2019-03-24 00:03:35 +0900

    format stack*

--- javascript/stack.test.ts
+++ javascript/stack.test.ts
@@ -2,27 +2,27 @@ import { Stack, createStack } from './stack';
 
 let stack: Stack<number>;
 beforeEach(() => {
-	stack = createStack<number>();
+  stack = createStack<number>();
 });
 
 test('empty', () => {
-	expect(stack.empty()).toBe(true);
+  expect(stack.empty()).toBe(true);
 });
 
 test('push and pop', () => {
-	stack.push(1);
-	expect(stack.empty()).toBe(false);
-	expect(stack.pop()).toBe(1);
-	expect(stack.empty()).toBe(true);
+  stack.push(1);
+  expect(stack.empty()).toBe(false);
+  expect(stack.pop()).toBe(1);
+  expect(stack.empty()).toBe(true);
 });
 
 test('stack overflow', () => {
-	stack = createStack(2);
-	stack.push(1);
-	stack.push(2);
-	expect(() => stack.push(3)).toThrowError('stack overflow');
+  stack = createStack(2);
+  stack.push(1);
+  stack.push(2);
+  expect(() => stack.push(3)).toThrowError('stack overflow');
 });
 
 test('stack underflow', () => {
-	expect(() => stack.pop()).toThrowError('stack underflow');
+  expect(() => stack.pop()).toThrowError('stack underflow');
 });
